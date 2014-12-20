ce42f3a1272ec584aebfbd3ec3f118498ef04fce
Tanya Pristupova <zyrk@zyrk.net>
2014-12-20 11:02:53 +0100

    Adds simple list box animation

---
+++ src/WpfAnimation/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using WpfAnimation.Annotations;
+
+namespace WpfAnimation
+{
+    public class MainWindowViewModel
+    {
+        
+    }
+}
