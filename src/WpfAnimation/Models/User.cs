650c9c07eb81c416bf7889ee36250e0e91f152fb
Tanya Pristupova <zyrk@zyrk.net>
2014-12-21 11:43:55 +0100

    Updates list box item template

---
+++ src/WpfAnimation/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace WpfAnimation.Models
+{
+    using System;
+
+    public class User
+    {
+        public User(string name, string commitId, string commitDescription, DateTime date)
+        {
+            Name = name;
+            CommitId = commitId;
+            CommitDescription = commitDescription;
+            Date = date;
+        }
+
+        #region Properties
+        public string Name { get; set; }
+        public string CommitId { get; set; }
+        public string CommitDescription { get; set; }
+        public DateTime Date { get; set; }
+        #endregion
+    }
+}
--- src/WpfAnimation/ViewModels/MainWindowViewModel.cs
+++ src/WpfAnimation/ViewModels/MainWindowViewModel.cs
@@ -2,24 +2,33 @@
 {
     using System;
     using System.Collections.ObjectModel;
+    using System.Collections.Specialized;
+    using System.Linq;
     using Catel.MVVM;
+    using Models;
 
     public class MainWindowViewModel: ViewModelBase
     {
-        public ObservableCollection<string> ListBoxItems { get; set; }
+        public ObservableCollection<User> ListBoxItems { get; set; }
 
         public MainWindowViewModel()
         {
-            ListBoxItems = new ObservableCollection<string>(){"item1", "item2", "item3"};
+            ListBoxItems = new ObservableCollection<User>()
+            {
+                new User("John","2738sd67", "Added remove button", DateTime.Today.Date),
+                new User("Steve","4s2374g5", "Added add button", DateTime.Today.Date),
+                new User("Suzanne","129sre8j", "Added list box animation", DateTime.Today.Date),
+            };
             AddItemCommand = new Command(OnAddItemCommandExecute);
         }
 
-        public string SelectedItem { get; set; }
+        public User SelectedItem { get; set; }
 
         public Command AddItemCommand { get; private set; }
         private void OnAddItemCommandExecute()
         {
-            ListBoxItems.Add(DateTime.Now.Ticks.ToString());
+//            ListBoxItems.Add(DateTime.Now.Ticks.ToString());
+            ListBoxItems.Insert(0, new User("Dave", "dh37491d", "Updated list box item style", DateTime.Today.Date));
         }
 
         #region DeleteListBoxItem command
